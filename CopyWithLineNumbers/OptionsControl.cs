fe87c82e5c83b3b363709dd409c7f1c9f0caa019
Masaru Tsuchiyama <m.tmatma@gmail.com>
2017-06-25 16:17:04 +0900

    issue #3 implement the setting option dialog

--- CopyWithLineNumbers/OptionsControl.cs
+++ CopyWithLineNumbers/OptionsControl.cs
@@ -13,12 +13,35 @@ namespace CopyWithLineNumbers
     public partial class OptionsControl : UserControl
     {
         private OptionsPageSetting settingOptionsPage;
+        readonly private string[] textsFormatsAtFirst = new string[] {
+            "FileName",
+            "FileName With Line Number",
+            "Absolute FilePath",
+            "Absolute FilePath With Line Number",
+        };
+        readonly private string[] textsFormatsOfLineNumbers = new string[] {
+            "Line Number",
+            "Line Number with FileName",
+            "Line Number with Absolute FilePath",
+        };
 
         public OptionsControl()
         {
             InitializeComponent();
+
+            foreach (string item in this.textsFormatsAtFirst)
+            {
+                this.comboBoxPathFormatAtFirst.Items.Add(item);
+            }
+            foreach (string item in this.textsFormatsOfLineNumbers)
+            {
+                this.comboBoxFormatOfTheLineNumbers.Items.Add(item);
+            }
+
+            this.comboBoxPathFormatAtFirst.SelectedIndex = 0;
+            this.comboBoxFormatOfTheLineNumbers.SelectedIndex = 0;
         }
-        
+
         /// <summary>
         /// Gets or sets the reference to the underlying OptionsPage object.
         /// </summary>
@@ -31,6 +54,22 @@ namespace CopyWithLineNumbers
             set
             {
                 settingOptionsPage = value;
+            }
+        }
+
+        private void checkBoxAddFileNameAtFirst_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBoxAddFileNameAtFirst.Checked)
+            {
+                this.comboBoxPathFormatAtFirst.Enabled = true;
+                this.textBoxAddFirst.Enabled = true;
+                this.textBoxAddLast.Enabled = true;
+            }
+            else
+            {
+                this.comboBoxPathFormatAtFirst.Enabled = false;
+                this.textBoxAddFirst.Enabled = false;
+                this.textBoxAddLast.Enabled = false;
             }
         }
     }
